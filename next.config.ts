523f500ac37218487d52c819e38b0938e28c7e5e
tiper2066 <tiper2066@gmail.com>
2025-07-31 10:46:48 +0900

    project setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* SVG를 컴포넌트처럼 사용하고 싶을 경우 @svgr/webpack 설정 */
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/i,
+            issuer: /\.[jt]sx?$/,
+            use: ['@svgr/webpack'],
+        });
+        return config;
+    },
 };
 
 export default nextConfig;
